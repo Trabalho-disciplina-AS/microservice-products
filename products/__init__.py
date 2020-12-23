00caa17d966edef777386e2a6912415f9ba799f0
adrilene <adrilenefonseca@gmail.com>
2020-12-23 00:37:15 -0300

    correct db

--- products/__init__.py
+++ products/__init__.py
@@ -2,16 +2,22 @@ from flask import Flask
 from flask_restful import Api
 from flask_mongoengine import MongoEngine
 from flask_cors import CORS
+import os
+import dns
 
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {
-    "db": "devices",
-    "host": "mongodb+srv://denis:arq1104@cluster0.a3msk.mongodb.net/test",
+    "db": "products",
+    "host": "mongodb+srv://denis:arq1104@cluster0.a3msk.mongodb.net/test"
 }
+
 db = MongoEngine(app)
 api = Api(app)
 CORS(app)
+
+# mongodb+srv://adrilene:arq2201@cluster0.a3msk.mongodb.net/test?retryWrites=true&w=majority&replicaSet=atlas-2nqljv-shard-0&ssl=true&authSource=admin
+#f"mongodb+srv://{os.environ['MONGODB_USERNAME']}:{os.environ['MONGODB_PASSWORD']}@{os.environ['MONGODB_HOSTNAME']}/{os.environ['MONGODB_DATABASE']}?ssl=false"
 
 
 from .models import product_model
